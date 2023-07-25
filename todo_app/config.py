4d7a261e60ec498bfa7b7778c386e57f71827d7b
enzojason <efd1808@gmail.com>
2023-07-25 12:44:16 -0300

    sub

---
+++ todo_app/config.py
@@ -0,0 +1,7 @@
+# modificar credenciales solo aqui
+credenciales = {'user': 'root',
+                'password': 'holafa',
+                'host': '127.0.0.1',
+                'database': 'todo_app'}
+# icono
+icono = "./img/todo.ico"
