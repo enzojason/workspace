768e0b5f3477ec3fd3b215f90bd814334d927d4e
enzo <efd1808@gmail.com>
2023-09-11 10:23:04 -0300

    11/9

--- TP3noe/config.py
+++
@@ -1,5 +0,0 @@
-class Config:
-    SERVER_NAME = "127.0.0.1:5000"
-    DEBUG = True
-    TEMPLATE_FOLDER = "templates/"
-    STATIC_FOLDER = "static_folder/"
--- TP3noe/run.py
+++
@@ -1,5 +0,0 @@
-from ejercicio1 import init_app
-app = init_app()
-
-if __name__ == "__main__":
-    app.run()
